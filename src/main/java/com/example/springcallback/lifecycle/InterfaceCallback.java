76e3afacb66fd9cef34daa0ea0d5f50decadee21
UK <hwangwonuk94@gmail.com>
2024-10-22 02:10:28 +0900

    feat: AppConfig로 Bean 수동등록

--- src/main/java/com/example/springcallback/lifecycle/InterfaceCallback.java
+++ src/main/java/com/example/springcallback/lifecycle/InterfaceCallback.java
@@ -3,19 +3,20 @@ package com.example.springcallback.lifecycle;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.example.springcallback.lifecycle")
 public class InterfaceCallback {
 
     public static void main(String[] args) {
         // Spring 컨테이너 생성 및 설정 클래스 등록
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(InterfaceCallback.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         // Bean 사용
         MyBean myBean = context.getBean(MyBean.class);
         myBean.doSomething();
 
+        System.out.println(myBean.getData());
         // 컨테이너 종료 (DisposableBean 호출됨)
         context.close();
+        System.out.println(myBean.getData());
     }
 
 }
--- src/main/java/com/example/springcallback/lifecycle/MyBean.java
+++ src/main/java/com/example/springcallback/lifecycle/MyBean.java
@@ -4,19 +4,35 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
-@Component
 public class MyBean implements InitializingBean, DisposableBean {
+
+    private String data;
+
+    public MyBean() {
+        System.out.println("Bean 생성자 호출");
+        System.out.println("data = " + data);
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
 
     // InitializingBean 인터페이스의 초기화 메서드
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("MyBean 초기화 - afterPropertiesSet() 호출됨");
+        System.out.println("data = " + data);
     }
 
     // DisposableBean 인터페이스의 종료 메서드
     @Override
     public void destroy() throws Exception {
         System.out.println("MyBean 종료 - destroy() 호출됨");
+        data = null;
     }
 
     public void doSomething() {
